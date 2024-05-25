009da138a75851cc07b23d2e3ca4c26b829212f8
triwjy <ztriwijaya@gmail.com>
2024-05-25 11:52:22 +0800

    add to cart function

--- src/PlantList.jsx
+++ src/PlantList.jsx
@@ -1,50 +1,102 @@
 import React, { useState } from 'react';
 import './ProductList.css';
+import { useDispatch } from 'react-redux';
+import { addItem } from './CreateSlice';
 
 const PlantList = () => {
   const baseImageURL = '/shoppingreact/images';
+  const dispatch = useDispatch();
+  const [disabledProducts, setDisabledProducts] = useState([]);
   const [plantsArray, setPlantsArray] = useState([
     {
       id: 1,
       category: 'Aromatic Fragrant Plants',
       plants: [
         {
-          id: 1,
+          id: 11,
           name: 'Lavender',
           imageUrl: `${baseImageURL}/lavender.jpg`,
           description: 'Lavenders description',
           cost: '10',
         },
         {
-          id: 2,
+          id: 12,
           name: 'Lavender',
           imageUrl: `${baseImageURL}/lavender.jpg`,
           description: 'Lavenders description',
           cost: '10',
         },
         {
-          id: 2,
+          id: 13,
           name: 'Lavender',
           imageUrl: `${baseImageURL}/lavender.jpg`,
           description: 'Lavenders description',
           cost: '10',
         },
         {
-          id: 1,
+          id: 14,
           name: 'Lavender',
           imageUrl: `${baseImageURL}/lavender.jpg`,
           description: 'Lavenders description',
           cost: '10',
         },
         {
-          id: 2,
+          id: 15,
           name: 'Lavender',
           imageUrl: `${baseImageURL}/lavender.jpg`,
           description: 'Lavenders description',
           cost: '10',
         },
         {
-          id: 2,
+          id: 16,
+          name: 'Lavender',
+          imageUrl: `${baseImageURL}/lavender.jpg`,
+          description: 'Lavenders description',
+          cost: '10',
+        },
+      ],
+    },
+    {
+      id: 2,
+      category: 'Air Purifying Plants',
+      plants: [
+        {
+          id: 21,
+          name: 'Lavender',
+          imageUrl: `${baseImageURL}/lavender.jpg`,
+          description: 'Lavenders description',
+          cost: '10',
+        },
+        {
+          id: 22,
+          name: 'Lavender',
+          imageUrl: `${baseImageURL}/lavender.jpg`,
+          description: 'Lavenders description',
+          cost: '10',
+        },
+        {
+          id: 23,
+          name: 'Lavender',
+          imageUrl: `${baseImageURL}/lavender.jpg`,
+          description: 'Lavenders description',
+          cost: '10',
+        },
+        {
+          id: 24,
+          name: 'Lavender',
+          imageUrl: `${baseImageURL}/lavender.jpg`,
+          description: 'Lavenders description',
+          cost: '10',
+        },
+        {
+          id: 25,
+          name: 'Lavender',
+          imageUrl: `${baseImageURL}/lavender.jpg`,
+          description: 'Lavenders description',
+          cost: '10',
+        },
+        {
+          id: 26,
           name: 'Lavender',
           imageUrl: `${baseImageURL}/lavender.jpg`,
           description: 'Lavenders description',
@@ -54,13 +106,17 @@ const PlantList = () => {
     },
   ]);
 
-  const handleAddToCart = () => {};
+  const handleAddToCart = (plant) => {
+    console.log('pressed');
+    dispatch(addItem(plant));
+    setDisabledProducts([...disabledProducts, plant.id]);
+  };
   return (
     <div className="product-grid">
       {plantsArray.map((category) => (
         <div key={category.id}>
           <h1>
-            <div>{category.category}</div>
+            <div className="product-category">{category.category}</div>
           </h1>
           <div className="product-list">
             {category.plants.map((plant) => (
@@ -75,8 +131,15 @@ const PlantList = () => {
                 </div>
                 <div className="product-price">${plant.cost}</div>
                 <div className="product-description">{plant.description}</div>
-                <button className="product-button" onClick={handleAddToCart}>
-                  Add to Cart
+                <button
+                  className={`product-button ${
+                    disabledProducts.includes(plant.id) ? 'disabled' : ''
+                  }`}
+                  onClick={() => handleAddToCart(plant)}
+                  disabled={disabledProducts.includes(plant.id)}
+                >
+                  {disabledProducts.includes(plant.id) ? 'Added' : 'Add'} to
+                  Cart
                 </button>
               </div>
             ))}
